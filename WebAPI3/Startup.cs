b8ab03b80df9dd05fddd9e7a63b220bf2a99cea7
Peter Mei <petermei@yahoo.com>
2019-10-06 16:02:29 -0700

    Add Swagger

--- WebAPI3/Startup.cs
+++ WebAPI3/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 using WebAPI3.Models;
+using Microsoft.OpenApi.Models;
 
 namespace WebAPI3
 {
@@ -27,6 +28,27 @@ namespace WebAPI3
         {
             services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
             services.AddControllers();
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo 
+                {   Title = "Core API", 
+                    Version = "v1",
+                    Description = "The ASP.NET Core 3.0 Web API",
+                    //TermsOfService = new Uri("https://example.com/terms"),
+                    //Contact = new OpenApiContact
+                    //{
+                    //    Name = "Peter Mei",
+                    //    Email = string.Empty,
+                    //    Url = new Uri("https://twitter.com/spboyer"),
+                    //},
+                    //License = new OpenApiLicense
+                    //{
+                    //    Name = "Use under LICX",
+                    //    Url = new Uri("https://example.com/license"),
+                    //}
+                });
+            });
             services.AddSingleton<IConfiguration>(Configuration);
         }
 
@@ -37,6 +59,18 @@ namespace WebAPI3
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Core API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
 
             app.UseRouting();
 
