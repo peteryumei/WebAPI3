69084b183046379517f323534345d2d63b6111e8
Peter Mei <petermei@yahoo.com>
2019-10-06 12:05:47 -0700

    Add ToDoItems controller with EF Core 3.0 and InMemoryDatabase

---
+++ WebAPI3/Models/TodoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI3.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<TodoItem> todoItems { get; set; }
+    }
+}
--- WebAPI3/Startup.cs
+++ WebAPI3/Startup.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using WebAPI3.Models;
 
 namespace WebAPI3
 {
@@ -24,6 +25,7 @@ namespace WebAPI3
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
             services.AddControllers();
             services.AddSingleton<IConfiguration>(Configuration);
         }
